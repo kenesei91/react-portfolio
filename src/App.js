196ecfb10fd0972f7a95ae19da133cee1c88ee69
Ken Ilo <keneilo91@yahoo.com>
2021-12-20 17:41:21 -0600

    portfolio page

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
     <div className="App">
       <Header />
       <Footer/>
-      <Project/>
     </div>
     
   );
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBCardLink } from 'mdb-react-ui-kit';
+import React from "react";
+import {
+  MDBCol,
+  MDBRow,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBCardLink,
+} from "mdb-react-ui-kit";
 
-function ProjectCards (props) {
+function ProjectCards(props) {
   return (
-    <MDBCard style={{ maxWidth: '22rem' }}>
-      <MDBCardImage src={props.image} position='top' alt={props.name} />
-      <MDBCardBody>
-        <MDBCardTitle>{props.name}</MDBCardTitle>
-        <MDBCardText>{props.topics}</MDBCardText>
-        <MDBCardLink href={props.github} target="_blank">GitHub Repo</MDBCardLink>
-        <MDBCardLink href={props.deploy} target="_blank">Deployed App</MDBCardLink>
-      </MDBCardBody>
-    </MDBCard>
+    <MDBRow className="row-2 cols-2  g-4">
+      <MDBCol>
+        <MDBCard style={{ maxWidth: "22rem" }}>
+          <MDBCardImage src={props.image} position="top" alt={props.name} />
+          <MDBCardBody>
+            <MDBCardTitle>{props.name}</MDBCardTitle>
+            <MDBCardText>{props.topics}</MDBCardText>
+            <MDBCardLink href={props.github} target="_blank">
+              GitHub Repo
+            </MDBCardLink>
+            <MDBCardLink href={props.deploy} target="_blank">
+              Deployed App
+            </MDBCardLink>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
   );
 }
 
