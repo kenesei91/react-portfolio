b58b3f2f26a955ae0fa5e55bd16d79fbbc4aa42d
Ken Ilo <keneilo91@yahoo.com>
2021-12-20 00:15:08 -0600

    created contact page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 //import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       {/* <Header /> */}
       <About />
       <Footer />
+      <Contact />
     </div>
   );
 }
