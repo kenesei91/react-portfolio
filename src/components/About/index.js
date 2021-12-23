01a1f935d6eecd7975cebd78a1dedb7f696f7566
Ken Ilo <keneilo91@yahoo.com>
2021-12-23 17:05:55 -0600

    tiny change

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,7 +10,7 @@ function About() {
             <div>
                 <img class="mb-5" id="avatar" src={photo} alt='profile'/>
                 <p>
-                Kenechukwu Ilochonwu, also known as Ken by his close friends worked as an Environmental Specialist for four years specifically tasked to ensure the protection and conservation of natural resources as well as establishing best practices to resolve complex issues. Ken believes achieving success in the workplace begins with having a strong mind, body and spirit which he channels through physical sports, competitive gaming, and travelling. 
+                Kenechukwu Ilochonwu, also known as Ken by his close friends worked as an Environmental Specialist for four years, specifically tasked to ensure the protection and conservation of natural resources as well as establishing best practices to resolve complex issues. Ken believes achieving success in the workplace begins with having a strong mind, body and spirit which he channels through physical sports, competitive gaming, and travelling. 
                 </p>
                 <p>
                 Ken is currently on a 6 month journey to becoming a full stack web developer, and by the end of this program, he would have acquired the necessary skills and knowledge to effeciently create and improve world wide web applications.
